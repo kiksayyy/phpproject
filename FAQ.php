9265b54d970b9a72e1a46e165c95215a2a0c89fe
MATININGJELEANE <matiningjeleane@gmail.com>
2023-05-31 21:16:30 +0800

    ASK me!

---
+++ FAQ.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+
+<header>
+            <a href="#" class="logo">
+                <img src="logo.jpg" alt="">
+            </a>
+        <nav>
+            <ul >
+                <li><a href="home.php" class="active">HOME</a></li>
+                <li><a href="menu.php">MENU</a></li>
+                <li><a href="home.php#about">ABOUT</a></li>
+                <li><a href="location.php">LOCATION</a></li>
+                <li><a href="reservation.php">RESERVATION</a></li>
+            </ul>
+            <li><a href="index.html" class="act">HOME</a></li>
+            <li><a href="signin.html" class="sign">Sign In</a></li>
+        </nav>
+        </header>
+    
+
+<div class="faq">
+    <h1>FREQUENTLY ASKED QUESTIONS</h1> 
+
+    <div class="faqs">
+        <br>
+        <h2>
+        How do I order thru Dayo delivery?
+        </h2>
+<p>
+        You may order thru the following ways:
+        </p>
+        <br>
+
+<p>Order thru the Dayo mobile app</p> 
+<p>#0961 317 5314 delivery accepts cash and credit card payments.</p> 
+<p>Visit the DayoDelivery.com website</p>   
+    </div>
+    <div class="faqs">
+        <br>
+        <h2>
+        Is Dayo delivery delivery available nationwide?
+        </h2>
+        <br>
+        <p>
+            No, Dayo delivery is only available around 20km radius.
+        </p>
+    </div>
+    <div class="faqs">
+        <br>
+        <h2>
+        How much is the delivery fee of Dayo Delivery?
+        </h2>
+        <br>
+        <p>
+        The Delivery Fee is 10% on top of the product Dine-in SRP.
+        </p>
+    </div>
+    <div class="faqs">
+        <br>
+        <h2>
+        How many minutes is the delivery serving time?
+        </h2>
+        <br>
+        <p>
+        Kindly expect your orders to arrive between 45mins to 1hour.
+        </p>
+    </div>
+
+    <div class="faqs">
+        <br>
+        <h2>
+        What’s the minimum and maximum order purchase for delivery?
+        </h2>
+        <br>
+        <p>
+        Minimum order of P200, Maximum of P5500 (inclusive of delivery fee)
+        </p>
+    </div>
+
+    <div class="faqs">
+        <br>
+        <h2>
+        What are the payment options available for delivery?
+        </h2>
+        <br>
+        <p>
+              Dayoedelivery.com or the Dayo Mobile App accepts debit, credit card payments through Pesopay and cashless payments thru GCash and Paymaya on orders.
+</p>
+<p>
+#0961 317 5314 delivery accepts cash and credit card payments.
+        </p>
+    </div>
+    <div class="faqs">
+        <br>
+        <h2>
+        Do you honor senior citizen / PWD discounts?
+        </h2>
+        <br>
+        <p>
+        Senior citizen and PWD discounts are honored thru Jollibee Delivery, as long as senior citizen / PWD ID is presented upon delivery.
+        </p>
+    </div>
+
+    <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <img src="images/logo-.png" alt="">
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>company</h6>
+                <ul>
+                    <li><a href="aboutus.php">about us</a></li>
+                    <li><a href="accessibility.php">Accessibility</a></li>
+                    <li><a href="privacy.php">privacy policy</a></li>
+                    <li><a href="condition.php">Condition of Use</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>get help</h6>
+                <ul>
+                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="location.php">delivery</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="franchising.php">Franchising</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="member.php">Group Members</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>follow us</h6>
+                <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+</div>
+</body>
+</html>
