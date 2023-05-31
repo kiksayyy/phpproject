f6b76038dd6f1bcaf6d46f7df2f6d56024454316
MATININGJELEANE <matiningjeleane@gmail.com>
2023-05-31 21:11:03 +0800

    instruction

---
+++ condition.php
@@ -0,0 +1,154 @@
+<!doctype html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Dayo</title>
+    <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!--navbar-->
+
+    <header>
+            <a href="#" class="logo">
+                <img src="logo.jpg" alt="">
+            </a>
+        <nav>
+            <ul >
+                <li><a href="home.php" class="active">HOME</a></li>
+                <li><a href="menu.php">MENU</a></li>
+                <li><a href="home.php#about">ABOUT</a></li>
+                <li><a href="location.php">LOCATION</a></li>
+                <li><a href="reservation.php">RESERVATION</a></li>
+            </ul>
+            <li><a href="index.html" class="act">HOME</a></li>
+            <li><a href="signin.html" class="sign">Sign In</a></li>
+        </nav>
+        </header>
+ 
+<div class="condition">
+    <h1>Condition of Use</h1>
+
+    <div class="condi">
+   <h2> How To Order OnlineHow To Order Online </h2>
+   <br>
+
+<p> Just register at our site www.dayodelivery.com and start ordering Dayo Restaurant food items for your loved ones here in the Marinduque.</p>
+
+<p> Order and PAY online in real-time using Dayo Restaurant Online Ordering!</p>
+
+<h3>
+Order online for:</h3>
+
+<p> PICK -UP (order food for pick-up at any Dayo Restaurant at Balogo,Boac, Marinduque)</p>
+<p> DELIVER LATER (pick your preferred date and time of delivery around 20km radius )</p>
+<p> DELIVER NOW (have your orders delivered asap anywhere within the 20km radius)</p>
+
+<p> Visit www.dayodelivery.com and surprise your loved ones anywhere in the Marinduque! You may also book a takeout or dine-in order for yourself.</p>
+
+<p> Soon you may use your credit card and other payment options to pay online. All prices on our site are in Philippines peso currency!</p>
+
+<p> Do try this innovative online service today!</p>
+<h3>
+Conditions Of Use
+</h3>
+<p> We understand that your order is meant for a very special person/occasion. We at Dayo Group Inc. only aim to deliver your order to your friends and loved ones within the Marinduque based on the specific date and time that you have chosen. To ensure proper and timely delivery, please read and follow our Conditions of Use below:</p>
+<h3>
+Online orders must be placed:
+</h3>
+<p>
+at least 1 hour in advance (for Deliver Now transactions and Pickup)</p>
+
+<p> at least 2 hours in advance (for Deliver Later transactions)</p>
+<p>at least 24 hours in advance (for Dine-in transactions)</p>
+<p> All transactions will be settled in PHP (Philippine Peso currency), and the minimum required purchase is worth PHP .</p>
+
+<p> Credit cards/ATM cards/other online payment facilities will be billed at the same time the order is placed.</p>
+<p> As proof of a fulfilled order, an electronic copy of the order confirmation letter will be emailed to the purchaser indicating the time, date, name of the recipient and other details of the order.</p>
+
+<p> Revisions and cancellations should be made:</p>
+
+<p> at most 5 minutes after order is placed (for Deliver Now transactions)</p>
+<p> at most 1 hour after order is placed (for Takeout and Deliver Later and Dine-in transactions)</p>
+
+<p> The sender is required to contact Maxs Restaurant Online team via email or telephone for cancellation or revision of online orders.</p>
+
+<p> Dayo Group Inc. Online reserves the right to reject order cancellations requested beyond required cancellation/revision lead times.</p>
+
+<p> Revisions/changes in the order are subject to confirmation and additional cost when applicable. Said changes/revisions may cause delay in delivery.</p>
+
+<p> Prices are subject to change without notice.</p>
+
+<p> The delivery area for provincial branches is within a 20 kilometers radius of the branch.</p>
+
+<p> Some provincial areas are considered unserviceable. Dayo Restaurant Online will update you if your recipient's address cannot be serviced by our delivery team.</p>
+
+<p> Takeout, Deliver Now and Deliver Later transactions can be processed during store business hours (earliest time to process an online order is 9:00am while the latest would be 8:30pm in the evening) </p>
+
+<p> Dine-in transactions will be processed the next day during store business hours (earliest time to process an online order is 9:00am while the latest would be 8:30pm in the evening) </p>
+<h3>
+Contact Us </h3>
+<p> Dayo Restaurant Online Team</p>
+<p> Tel No: 0961 317 5314 from Monday to Sunday (8:00am to 8:30PM)</p>
+<p> Email: dayo2020@gmail.com</p>
+
+    </div>
+</div>
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <img src="images/logo-.png" alt="">
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>company</h6>
+                <ul>
+                    <li><a href="aboutus.php">about us</a></li>
+                    <li><a href="accessibility.php">Accessibility</a></li>
+                    <li><a href="privacy.php">privacy policy</a></li>
+                    <li><a href="condition.php">Condition of Use</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>get help</h6>
+                <ul>
+                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="location.php">delivery</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="franchising.php">Franchising</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="member.php">Group Members</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>follow us</h6>
+                <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
