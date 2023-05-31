a92010af30650e27db688b81d90391ed07821d25
MATININGJELEANE <matiningjeleane@gmail.com>
2023-05-31 21:12:41 +0800

    sample feedback

---
+++ reviews.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Dayo</title>
+    <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!--navbar-->
+
+    <header>
+            <a href="#" class="logo">
+                <img src="logo.jpg" alt="">
+            </a>
+        <nav>
+            <ul >
+                <li><a href="home.php" class="active">HOME</a></li>
+                <li><a href="menu.php">MENU</a></li>
+                <li><a href="home.php#about">ABOUT</a></li>
+                <li><a href="location.php">LOCATION</a></li>
+                <li><a href="reservation.php">RESERVATION</a></li>
+            </ul>
+            <li><a href="index.html" class="act">HOME</a></li>
+            <li><a href="signin.html" class="sign">Sign In</a></li>
+        </nav>
+        </header>
+<div class="rev">
+    <h1>Reviews</h1>
+
+    <div class="reviews">
+<br>
+    <img src="images/about.jpg" alt="">
+    <h2>Lebron James</h2>
+    <p>"their service and hospitality are excellent. One of the staff working with her smiles and making us confortable meanwhile i was staying there."</p>
+    </div>
+</div>
+<div class="reviews">
+<br>
+    <img src="images/kobe.webp" alt="">
+    <h2>Kobe Braynt</h2>
+    <p>"Excellent service! They helped us surprise my grandfather with a delicious Lasagna when my grandfather he thought we were only having coffee."</p>
+    </div>
+</div>
+<div class="reviews">
+<br>
+    <img src="images/durant.jpg" alt="">
+    <h2>Kevin Durant</h2>
+    <p>"Excellent food. Service is quick and the staff is so accommodating and efficient. Good music in the background completes the fine dining experience. Keep up the good work guys!"</p>
+    </div>
+</div>
+<div class="reviews">
+<br>
+    <img src="images/about.jpg" alt="">
+    <h2>Julius Randle</h2>
+    <p>"I visited this restaurant second time, still great in food and service, can't wait to bring my friends so that they will feel the awesomeness of this restaurant.
+
+"</p>
+    </div>
+    <div class="reviews">
+<br>
+
+    <h2>Write A Review</h2>
+    <input type="text">
+    <br>
+    <button>Submit</button>
+    </div>
+</div>
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <img src="images/logo-.png" alt="">
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>company</h6>
+                <ul>
+                    <li><a href="aboutus.php">about us</a></li>
+                    <li><a href="accessibility.php">Accessibility</a></li>
+                    <li><a href="privacy.php">privacy policy</a></li>
+                    <li><a href="condition.php">Condition of Use</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>get help</h6>
+                <ul>
+                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="location.php">delivery</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="franchising.php">Franchising</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="member.php">Group Members</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>follow us</h6>
+                <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
