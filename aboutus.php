cb21ea6049e6f12735e9822e21579a16f8273259
MATININGJELEANE <matiningjeleane@gmail.com>
2023-05-31 21:03:24 +0800

    Short information to us

---
+++ aboutus.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Dayo</title>
+    <link rel="shortcut icon" href="logo.jpg" type="image/x-icon">
+    <link rel="stylesheet" href="css/style.css?v=<?php echo time(); ?>">
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+    <script src="https://kit.fontawesome.com/yourcode.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!--navbar-->
+
+
+    <header>
+            <a href="#" class="logo">
+                <img src="logo.jpg" alt="">
+            </a>
+        <nav>
+            <ul >
+                <li><a href="home.php" class="active">HOME</a></li>
+                <li><a href="menu.php">MENU</a></li>
+                <li><a href="home.php#about">ABOUT</a></li>
+                <li><a href="location.php">LOCATION</a></li>
+                <li><a href="reservation.php">RESERVATION</a></li>
+            </ul>
+            <li><a href="index.html" class="act">HOME</a></li>
+            <li><a href="signin.html" class="sign">Sign In</a></li>
+        </nav>
+        </header>
+
+<div class="aboutus">
+    <div class="abouts">
+        <h1>About Us</h1>
+        <p>Welcome to Dayo Restaurant, where culinary excellence meets warm hospitality. Nestled in the heart of the Philippines, we are a premier dining destination that celebrates the art of food and the joy of sharing a meal with loved ones.
+</p>
+<p>
+At Dayo Restaurant, our talented team of chefs is dedicated to crafting exquisite dishes that tantalize your taste buds and ignite your senses. We draw inspiration from global flavors and carefully select the finest ingredients to create a diverse menu that caters to a variety of palates.
+</p>
+<p>
+Our commitment to quality is unwavering. We source our ingredients locally whenever possible, supporting local farmers and artisans. This allows us to serve you the freshest, seasonal produce, ensuring that every dish bursts with flavor and showcases the best of what the region has to offer.
+</p>
+<p>
+Step into our inviting and elegant space, where contemporary design merges with a warm and welcoming atmosphere. Whether you're seeking an intimate dinner for two or planning a memorable celebration with friends and family, our restaurant offers a relaxed and sophisticated setting that sets the stage for an exceptional dining experience.
+</p>
+<p>
+At Dayo Restaurant, we believe that dining is not just about the food; it's about creating unforgettable moments. Our attentive and knowledgeable staff is committed to providing impeccable service, guiding you through our menu, and ensuring that every aspect of your visit exceeds your expectations.
+</p>
+<p>
+We take pride in curating an extensive wine and beverage selection that perfectly complements our culinary offerings. Our sommeliers have carefully curated a diverse collection of wines, beers, and spirits from around the world, allowing you to discover new flavors and elevate your dining experience.
+</p>
+<p>
+Whether you're joining us for a leisurely lunch, a romantic dinner, or a special occasion, Dayo Restaurant is the perfect destination for those seeking an extraordinary culinary journey. We invite you to savor the flavors, indulge in the ambiance, and create lasting memories with us.
+</p>
+<p>
+Thank you for choosing Dayo Restaurant. We look forward to serving you and delighting your senses with our exceptional cuisine and warm hospitality.</p>
+    </div>
+
+</div>
+
+
+<footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <img src="images/logo-.png" alt="">
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>company</h6>
+                <ul>
+                    <li><a href="aboutus.php">about us</a></li>
+                    <li><a href="accessibility.php">Accessibility</a></li>
+                    <li><a href="privacy.php">privacy policy</a></li>
+                    <li><a href="condition.php">Condition of Use</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>get help</h6>
+                <ul>
+                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="location.php">delivery</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="franchising.php">Franchising</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="member.php">Group Members</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>follow us</h6>
+                <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
