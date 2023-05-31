5c1091558fdb5754a004722cd419058e0bc7b10d
MATININGJELEANE <matiningjeleane@gmail.com>
2023-05-31 21:07:34 +0800

    keep private

---
+++ privacy.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+            <a href="#" class="logo">
+                <img src="logo.jpg" alt="">
+            </a>
+        <nav>
+            <ul >
+                <li><a href="home.php" class="active">HOME</a></li>
+                <li><a href="menu.php">MENU</a></li>
+                <li><a href="home.php#about">ABOUT</a></li>
+                <li><a href="location.php">LOCATION</a></li>
+                <li><a href="reservation.php">RESERVATION</a></li>
+            </ul>
+            <li><a href="index.html" class="act">HOME</a></li>
+            <li><a href="signin.html" class="sign">Sign In</a></li>
+        </nav>
+        </header>
+
+
+
+
+    <div class="privacy">
+        <h1>
+                       Dayo Foods Corporation  
+           Corporate Website Privacy Policy
+        </h1>
+<p>
+Dayo Foods Corporation (the "Company") is committed to the protection of the privacy of your personal information as we are conscious of our responsibilities as a "personal information controller" under Republic Act No. 10173 or the Data Privacy Act of 2012.
+</p>
+<p>
+You can visit most pages on this website without the need of giving personal information; however, there are certain instances when we will passively collect non-personally identifiable information in order to give you an outstanding browsing experience. The information we collect and our policy regarding its collection and storage are discussed in detail below. We believe it is important for you to know how we treat and protect your information.
+</p>
+<p>
+The terms of this Privacy Policy apply to all users of this website and to all relevant activities of the Company. Should the Company launch other online initiatives that will require the collection of personal information or will entail another way of collecting and storing data, the Company reserves the right to formulate a more specific privacy policy for these initiatives.
+</p>
+<p>
+If you do not agree with the terms of this Privacy Policy, you should immediately stop the use of this website. Your use of this website shall be construed as your acknowledgement and acceptance of the terms of this Privacy Policy.
+</p>
+<h2>
+What we Collect
+</h2>
+<p>
+This website only collects non-personally identifiable information such as information about your browser type, browser language, internet protocol (IP) address, geolocation, server name, date and time of accessing this website, the pages you access while you are at this website, and the address of the website from which you linked this website from. In some instances, we may collect information through the use of cookies. If we are going to use cookies, we shall declare it expressly in this website.
+</p>
+<p>
+The information collected, coupled with your frequent visits, will impact your use of the website. Collection is necessary as it allows us to analyze our performance in order to tailor a better user experience for you.
+</p>
+<h2>
+How we Collect
+</h2>
+<p>
+Our website may use or install cookies to help customize your experience upon each visit. These cookies are small packets of data sent from a website and stored in your computer by your browser while you are browsing. Cookies allow websites to remember certain information or to record your browsing activity. This, in turn, allows us to respond to you as an individual. Our website can adapt its operations to your needs, likes and dislikes by gathering and remembering information about your individual preferences.
+</p>
+<p>
+Overall, cookies help us provide you with a better website by enabling us to monitor which pages you find useful and which you do not. In no way do cookies give us access to your computer or any information about you, other than the data you choose to share with us. Furthermore, any information that we gather will not be disclosed outside of our company, affiliates, third party agents, business partners, nor will it be used for unsolicited communication.
+</p>
+<p>
+You do not need to enable cookies to navigate this website. If you prefer not to receive cookies while browsing our website, you can adjust your browser to alert you of the presence of cookies so that you may accept or refuse to accept the cookies while browsing. You can further adjust your browser to refuse all cookies that may be present in a website. By disabling or refusing to receive cookies, however, you may encounter some difficulties in using some parts of the website.
+</p>
+
+<h2>
+Security, Storage, and Disposal
+</h2>
+<p>
+We are committed to protecting the security of your information. We use a variety of secure physical, electronic and managerial procedures to help protect your information from unauthorized access, use or disclosure. Your information will be stored and processed in a secure internal database which shall be maintained and controlled exclusively by the Company. Access to the database may only be given to the Company's affiliates, subsidiaries and authorized agents.
+</p>
+<p>
+We shall retain the information we collect only for as long as necessary for the fulfillment of the purposes stated in this Privacy Policy. We generally do not store this information for a period longer than ten years after the stated purposes have been fulfilled; after which, the information will be disposed of in accordance with the relevant data privacy laws, regulations and company guidelines.
+</p>
+<h2>
+Links to Other Websites
+</h2>
+<p>
+Our website may contain links to other websites of interest. Please note that we do not have any control over the content of these other websites. Once you leave our website by clicking on these links, you acknowledge that the Company cannot be held responsible for the protection and privacy of any information you provide these other websites. We highly encourage you to exercise prudence and look at the privacy notices/policies of the other websites that you visit.
+</p>
+<h2>
+Updating of the Privacy Policy
+</h2>
+<p>
+We may update and/or revise this Privacy Policy from time to time. Changes to this Privacy Policy shall be posted on our homepage so that you are always aware of any changes to how we collect and process information. The latest version of this policy shall be determined by checking the date shown in the "Last Updated" tab of this Privacy Policy.
+</p>
+<h2>
+Inquiries
+</h2>
+<p>
+If you have any questions regarding this Privacy Policy, or our use of your information, please contact us through:
+<p>
+The Data Protection Officer - Dayo Foods Corporation
+</p>
+<p>
+dayo2020@gmail.com
+</p>
+<p>
+0961 317 5314
+</p>
+<p>
+Balogo, Boac, Marinduque
+</p>
+<h2>
+Last updated: May 21, 2020
+        </h2>
+    </div>
+
+    <footer class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col">
+                <ul>
+                    <img src="images/logo-.png" alt="">
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>company</h6>
+                <ul>
+                    <li><a href="aboutus.php">about us</a></li>
+                    <li><a href="accessibility.php">Accessibility</a></li>
+                    <li><a href="privacy.php">privacy policy</a></li>
+                    <li><a href="condition.php">Condition of Use</a></li>
+                    <li><a href="reviews.php">Reviews</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>get help</h6>
+                <ul>
+                    <li><a href="FAQ.php">FAQ</a></li>
+                    <li><a href="location.php">delivery</a></li>
+                    <li><a href="reservation.php">reservations</a></li>
+                    <li><a href="franchising.php">Franchising</a></li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li><a href="member.php">Group Members</a></li>
+                </ul>
+            </div>
+            <div class="footer-col">
+                <h6>follow us</h6>
+                <div class="social-links">
+                    <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                    <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</footer>
+
+
+
+
+
+</body>
+</html>
